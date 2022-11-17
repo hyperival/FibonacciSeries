fd39bdf80cd4da195eee473bb7d8b5c46291cf0f
Hyperival <sanjaygautam779@gmail.com>
2022-11-17 20:13:27 +0530

    fibonacci series code is added

---
+++ FibonacciSeries/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSeries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            int c;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            for (int i = 0; i < 10; i++)
+            {
+                c = a + b;
+                Console.WriteLine(c);
+                a = b;
+                b = c;
+            }
+                Console.ReadLine();
+            
+        }
+    }
+}
